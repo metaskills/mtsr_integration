81be4ef9b96a360732f3b504fcf01b078e320dba
Ken Collins <ken@metaskills.net>
2013-11-25 09:11:35 -0500

    Add failing test case.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test "the truth" do
-    assert true
+  let(:email) { 'ken@metaskills.net' }
+  let(:user)  { User.new.tap { |u| u.email = email } }
+
+  it 'has an email' do
+    user.email.must_equal email
   end
 
   
