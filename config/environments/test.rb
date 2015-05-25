5cd4daa89a5ab76032458de2f96ba7e4731a7789
Ken Collins <ken@metaskills.net>
2015-05-25 13:09:08 -0400

    Update to Rails 4.2.1

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,7 @@
 MtsrIntegration::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.minitest_spec_rails.mini_shoulda = true
+  config.active_support.test_order = :random
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
